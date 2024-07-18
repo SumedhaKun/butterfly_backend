534e2027b016353bf272542e8d9f6afe443b048d
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-07-18 16:42:08 -0700

    added model test cases

--- social/posts_app/tests.py
+++ social/posts_app/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
-
+from .models import Post,User, Comment
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 # Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.image = Image.new('RGB', (100, 100), color='blue')
+        img_io = BytesIO()
+        self.image.save(img_io, format='JPEG')
+        self.image_file = ContentFile(img_io.read(), 'test_image.jpg')
+
+    def test_model_creation(self):
+        post=Post.objects.create(title="Test Title",data="Test Data", image=self.image_file, date="2024-07-03" )
+        self.assertEqual(post.image.name, 'test_image.jpg')
+        self.assertEqual(post.title,"Test Title")
+        self.assertEqual(post.data,"Test Data")
+        self.assertEqual(post.likes, 0)
+        self.assertEqual(post.date,"2024-07-03")
+        self.assertEqual(post.caption,"Loading...")
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        self.image = Image.new('RGB', (100, 100), color='blue')
+        img_io = BytesIO()
+        self.image.save(img_io, format='JPEG')
+        self.image_file = ContentFile(img_io.read(), 'test_image.jpg')
+
+    def test_model_creation(self):
+        post=Post.objects.create(title="Test Title",data="Test Data", image=self.image_file, date="2024-07-03" )
+        comment=Comment.objects.create(date="2024-07-03",content="Test Content", post=post)
+        self.assertEqual(comment.post.title,"Test Title")
+        self.assertEqual(comment.date,"2024-07-03")
+        self.assertEqual(comment.content,"Test Content")
+        self.assertEqual(comment.likes,0)
+
+class UserTestCase(TestCase):
+    def test_model_creation(self):
+        user=User.objects.create(username="test_name",email="test@gmail.com",password="password")
+        self.assertEqual(user.username,"test_name")
+        self.assertEqual(user.email,"test@gmail.com")
+
+
--- social/posts_app/views.py
+++ social/posts_app/views.py
@@ -1,19 +1,15 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.views import APIView  
-# Create your views here.
 from .models import Post
 from .models import Comment
 from django.contrib.auth.models import User
 from .serializers import PostSerializer, UserSerializer, CommentSerializer
 from rest_framework import generics
 import rest_framework.permissions
-from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login, logout
-from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -25,6 +21,8 @@ import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
 
+
+# User views:
 
 @csrf_exempt 
 @api_view(['POST'])
@@ -39,6 +37,66 @@ def create_user(request):
         user.save()
         return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED,headers=headers)
 
+@permission_classes([AllowAny])
+class UserListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+@ensure_csrf_cookie
+@permission_classes([rest_framework.permissions.IsAuthenticated])
+@api_view(['GET'])
+def get_authenticated_user(request):
+    user = request.user
+    headers={"Access-Control-Allow-Origin":"http://127.0.0.1:3000", "Access-Control-Allow-Credentials": "true",}
+    if user.is_authenticated:
+        data = {
+            'username': user.username,
+            'email': user.email,
+        }
+        return Response(data, status=200,headers=headers)
+    else:
+        return Response({'error': 'User not authenticated'}, status=401,headers=headers)
+
+@csrf_exempt 
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    if request.method == 'POST':
+        username = request.data["username"]
+        password = request.data["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            # login(request, user)
+            print(user.get_username())
+            access_token = AccessToken.for_user(user)
+            return Response({'token': str(access_token)},status=status.HTTP_200_OK)
+            
+        else:
+            return Response("username or password is incorrect", status=status.HTTP_400_BAD_REQUEST)
+        
+@permission_classes([rest_framework.permissions.IsAuthenticated])
+@api_view(['GET'])
+def get_user_by_key(request,pk):
+    try:
+        user = User.objects.get(pk=pk)
+        followers=user.profile.followers
+        followers = UserSerializer(followers, many=True)
+
+        following=user.profile.following
+        following = UserSerializer(following, many=True)
+        data = {
+            'username': user.username,
+            'email': user.email,
+            'followers': followers.data,
+            'following':following.data
+        }
+        return Response(data,status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
 @api_view(['PATCH'])
 def follow_user(request,pk):
     user_to_follow=User.objects.get(pk=pk)
@@ -65,70 +123,14 @@ def get_following(request):
     serializer = UserSerializer(following, many=True)
     return Response(serializer.data)
 
-
+@api_view(['DELETE'])
 @permission_classes([AllowAny])
-class UserListView(APIView):
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-    
-@permission_classes([rest_framework.permissions.IsAuthenticated])
-@api_view(['GET'])
-def get_user_by_key(request,pk):
-    try:
-        user = User.objects.get(pk=pk)
-        followers=user.profile.followers
-        followers = UserSerializer(followers, many=True)
+def delete_user(request,pk):
+    user=User.objects.get(pk=pk)
+    user.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-        following=user.profile.following
-        following = UserSerializer(following, many=True)
-        data = {
-            'username': user.username,
-            'email': user.email,
-            'followers': followers.data,
-            'following':following.data
-        }
-        return Response(data,status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-@ensure_csrf_cookie
-@permission_classes([rest_framework.permissions.IsAuthenticated])
-@api_view(['GET'])
-def get_authenticated_user(request):
-    user = request.user
-    headers={"Access-Control-Allow-Origin":"http://127.0.0.1:3000", "Access-Control-Allow-Credentials": "true",}
-    if user.is_authenticated:
-        data = {
-            'username': user.username,
-            'email': user.email,
-        }
-        return Response(data, status=200,headers=headers)
-    else:
-        return Response({'error': 'User not authenticated'}, status=401,headers=headers)
-@csrf_exempt 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def login_user(request):
-    if request.method == 'POST':
-        username = request.data["username"]
-        password = request.data["password"]
-        user = authenticate(request, username=username, password=password)
-        if user:
-            # login(request, user)
-            print(user.get_username())
-            access_token = AccessToken.for_user(user)
-            return Response({'token': str(access_token)},status=status.HTTP_200_OK)
-            
-        else:
-            return Response("username or password is incorrect", status=status.HTTP_400_BAD_REQUEST)
-               
-@api_view(['POST'])
-def logout_view(request):
-    logout(request)
-    return Response(status=status.HTTP_200_OK)
+# Post Views:
 
 def save_image(image):
     aws_access_key_id = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
@@ -176,6 +178,35 @@ def get_posts_by_user(request,pk):
     serializer = PostSerializer(posts, many=True)
     return JsonResponse(serializer.data, safe=False,status=status.HTTP_200_OK)
 
+@api_view(['PATCH'])
+def update_post_likes(request,pk):
+    post=Post.objects.get(pk=pk)
+    post.likes+=1
+    post.save()
+    return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['PATCH'])
+def update_caption(request,pk):
+    post=Post.objects.get(pk=pk)
+    post.caption=request.data["caption"]
+    post.save()
+    return Response(status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+@permission_classes([AllowAny])
+def delete_post(request,pk):
+    post=Post.objects.get(pk=pk)
+    post.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#Comment views:
+
+class CommentListView(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
 @api_view(['GET'])
 def get_comments_by_post(request,pk):
     post=Post.objects.get(pk=pk)
@@ -200,39 +231,6 @@ def update_comment_likes(request,pk):
     comment.save()
     return Response(status=status.HTTP_200_OK)
 
-@api_view(['PATCH'])
-def update_post_likes(request,pk):
-    post=Post.objects.get(pk=pk)
-    post.likes+=1
-    post.save()
-    return Response(status=status.HTTP_200_OK)
-
-
-class CommentListView(generics.ListAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-@api_view(['PATCH'])
-def update_caption(request,pk):
-    post=Post.objects.get(pk=pk)
-    post.caption=request.data["caption"]
-    post.save()
-    return Response(status=status.HTTP_200_OK)
-    
-
-@api_view(['DELETE'])
-@permission_classes([AllowAny])
-def delete_user(request,pk):
-    user=User.objects.get(pk=pk)
-    user.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['DELETE'])
-@permission_classes([AllowAny])
-def delete_post(request,pk):
-    post=Post.objects.get(pk=pk)
-    post.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['DELETE'])
 @permission_classes([AllowAny])
@@ -240,6 +238,10 @@ def delete_comment(request,pk):
     comment=Comment.objects.get(pk=pk)
     comment.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+
+
 
 
 
