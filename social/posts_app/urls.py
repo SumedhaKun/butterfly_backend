9ced75e4bda72ccdf330e28bb960b852992e08c9
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-09-13 09:38:40 -0700

    removed logout url

--- social/posts_app/urls.py
+++ social/posts_app/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('api/posts/', PostListView.as_view(), name='post-list'),
     path('api/register/', views.create_user, name='create_user'),
     path('api/login/', views.login_user, name='login_user'),
-    path('api/logout/', views.logout_view, name='logout_user'),
     path('api/users/', UserListView.as_view(), name='user-list'),
     path('api/user/<int:pk>/', views.get_user_by_key, name='user'),
     path('api/user/', views.get_authenticated_user, name='get_authenticated_user'),
